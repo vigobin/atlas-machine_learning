4cda4daeb0b66b4f6168190823fa8efeea0d7c0c
vigobin <vgobin@outlook.com>
2024-01-05 10:34:58 -0600

    Initialize q-table

--- reinforcement_learning/q_learning/1-q_init.py
+++ reinforcement_learning/q_learning/1-q_init.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 """Initialize Q-table"""
+import numpy as np
 
 
 def q_init(env):
     """Initializes the Q-table:
     env is the FrozenLakeEnv instance.
     Returns: the Q-table as a numpy.ndarray of zeros."""
+    q_table = np.zeros([env.observation_space.n, env.action_space.n])
+    return q_table
