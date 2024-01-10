631565493f89f923210738ce76f5dfb985d05cab
vigobin <vgobin@outlook.com>
2024-01-10 16:11:50 -0600

    load to test

--- reinforcement_learning/deep_q_learning/play.py
+++ reinforcement_learning/deep_q_learning/play.py
@@ -1,2 +1,6 @@
 #!/usr/bin/env python3
 """Scrip to display a game played by the agent trained by train.py."""
+
+dqn.load_weights('/content/drive/MyDrive/dqn/weights.h5')
+
+dqn.test(env, nb_episodes=5, visualize=True)
