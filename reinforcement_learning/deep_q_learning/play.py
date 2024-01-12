761abce68eb58565cf3d58f5a4a8e2399adb50c1
vigobin <vgobin@outlook.com>
2024-01-11 18:35:12 -0600

    Finally wieghts saved and loaded as needed

--- reinforcement_learning/deep_q_learning/play.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-"""Scrip to display a game played by the agent trained by train.py."""
-
-dqn.load_weights('/content/drive/MyDrive/dqn/weights.h5')
-
-dqn.test(env, nb_episodes=5, visualize=True)
