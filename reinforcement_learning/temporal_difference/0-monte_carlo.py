3900085d7115e680a31429abf1c024f222337fec
vigobin <vgobin@outlook.com>
2024-01-17 11:59:48 -0600

    Added monte carlo algo

--- reinforcement_learning/temporal_difference/0-monte_carlo.py
+++ reinforcement_learning/temporal_difference/0-monte_carlo.py
@@ -13,3 +13,22 @@ def monte_carlo(env, V, policy, episodes=5000, max_steps=100,
         alpha is the learning rate.
         gamma is the discount rate.
         Returns: V, the updated value estimate."""
+    for _ in range(episodes):
+        state = env.reset()
+        done = False
+        steps = 0
+        rewards = []
+        states = []
+        while not done and steps < max_steps:
+            action = policy(state)
+            next_state, reward, done, _ = env.step(action)
+            states.append(state)
+            rewards.append(reward)
+            state = next_state
+            steps += 1
+
+        G = 0
+        for t in range(steps-1, -1, -1):
+            G = gamma * G + rewards[t]
+            V[states[t]] = V[states[t]] + alpha * (G - V[states[t]])
+    return V
