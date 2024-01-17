4572f5f50e59cefa53cedb0f22becdf5955d5aa3
vigobin <vgobin@outlook.com>
2024-01-17 12:02:19 -0600

    notebook

--- reinforcement_learning/temporal_difference/0-monte_carlo.py
+++ reinforcement_learning/temporal_difference/0-monte_carlo.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 """Monte Carlo function"""
 
+import numpy as np
+
+
 def monte_carlo(env, V, policy, episodes=5000, max_steps=100,
                 alpha=0.1, gamma=0.99):
     """Performs the Monte Carlo algorithm:
