2e8b63be0a8d117b5914724e6b0db0753a22c84e
vigobin <vgobin@outlook.com>
2024-01-22 11:07:39 -0600

    implement training

--- reinforcement_learning/policy_gradients/train.py
+++ reinforcement_learning/policy_gradients/train.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 """Implement the training"""
 
+import numpy as np
+from policy_gradient import policy_gradient
+
 
 def train(env, nb_episodes, alpha=0.000045, gamma=0.98):
     """function that implements a full training
@@ -10,3 +13,40 @@ def train(env, nb_episodes, alpha=0.000045, gamma=0.98):
         gamma: the discount factor.
         Return: all values of the score
             (sum of all rewards during one episode loop)."""
+    n_states = env.observation_space.shape[0]
+    n_actions = env.action_space.n
+    weight = np.random.rand(n_states, n_actions)
+
+    scores = []
+
+    for episode in range(nb_episodes + 1):
+        state = env.reset()[None, :]
+        grads = []
+        rewards = []
+        score = 0
+
+        done = False
+
+        while not done:
+            action, grad = policy_gradient(state, weight)
+
+            next_state, reward, done, _ = env.step(action)
+
+            grads.append(grad)
+            rewards.append(reward)
+
+            score += reward
+            state = next_state[None, :]
+
+        scores.append(score)
+
+        for i in range(len(rewards) - 2, -1, -1):
+            rewards[i] += rewards[i + 1] * gamma
+
+        for grad, reward in zip(grads, rewards):
+            weight += alpha * grad * reward
+
+        print("Episode: {}, Score: {}".format(
+            episode, score), end="\r", flush=False)
+
+    return scores
