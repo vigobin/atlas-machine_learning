1758c948a7ec6a77fa34e3d75eba0b956e6f35a3
vigobin <vgobin@outlook.com>
2024-01-29 16:23:47 -0600

    added flipped function

--- pipeline/data_augmentation/0-flip.py
+++ pipeline/data_augmentation/0-flip.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 """Flip function"""
+import tensorflow as tf
+
 
 def flip_image(image):
     """Flips an image horizontally:
         image is a 3D tf.Tensor containing the image to flip.
         Returns the flipped image."""
-    
+    flipped = tf.image.flip_left_right(image)
+    return flipped
