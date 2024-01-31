8c63ab0bb680c1795ddf81625d14b080d7e162a8
vigobin <vgobin@outlook.com>
2024-01-31 15:41:34 -0600

    rotate function

--- pipeline/data_augmentation/2-rotate.py
+++ pipeline/data_augmentation/2-rotate.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""Rotate function"""
 import tensorflow as tf
 
 
@@ -6,3 +7,4 @@ def rotate_image(image):
     """Rotates an image by 90 degrees counter-clockwise:
         image is a 3D tf.Tensor containing the image to rotate.
         Returns the rotated image."""
+    return tf.image.rot90(image)
