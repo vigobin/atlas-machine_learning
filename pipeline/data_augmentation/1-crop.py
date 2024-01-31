2d2091522c4f8ac8d97b4ff537163b5cec60038c
vigobin <vgobin@outlook.com>
2024-01-31 15:26:28 -0600

    added crop function

--- pipeline/data_augmentation/1-crop.py
+++ pipeline/data_augmentation/1-crop.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 """Crop function"""
+import tensorflow as tf
+
 
 def crop_image(image, size):
     """Performs a random crop of an image:
         image is a 3D tf.Tensor containing the image to crop.
         size is a tuple containing the size of the crop.
         Returns the cropped image."""
+    cropped_img = tf.image.random_crop(image, size=size)
+    return cropped_img
