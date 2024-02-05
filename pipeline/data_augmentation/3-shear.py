1a96b68baffe2cd3e92a2b500bd9bb103efaeded
vigobin <vgobin@outlook.com>
2024-02-05 15:11:56 -0600

    added shear

--- pipeline/data_augmentation/3-shear.py
+++ pipeline/data_augmentation/3-shear.py
@@ -3,8 +3,14 @@
 import tensorflow as tf
 
 
-def shear_image(image, intensity): 
+def shear_image(image, intensity):
     """Randomly shears an image:
         image is a 3D tf.Tensor containing the image to shear.
         intensity is the intensity with which the image should be sheared.
         Returns the sheared image."""
+    return tf.keras.preprocessing.image.random_shear(image,
+                                                     intensity,
+                                                     row_axis=1,
+                                                     col_axis=0,
+                                                     channel_axis=2,
+                                                     )
