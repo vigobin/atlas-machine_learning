bd49de509f3f1b779c09fe5432712f9f0965f5a3
vigobin <vgobin@outlook.com>
2024-02-14 14:59:57 -0600

    Initial commit

---
+++ pipeline/pandas/0-from_numpy.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""From Numpy"""
+
+
+def from_numpy(array):
+    """Creates a pd.DataFrame from a np.ndarray:
+        array is the np.ndarray from which you should create the pd.DataFrame.
+    The columns of the pd.DataFrame should be labeled in alphabetical order
+        and capitalized. There will not be more than 26 columns.
+    Returns: the newly created pd.DataFrame."""
