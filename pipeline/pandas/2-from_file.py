28af1e7c1cd7a68087e3c7535e4b5a85c4e7bc84
vigobin <vgobin@outlook.com>
2024-02-14 19:44:06 -0600

    Task 2

---
+++ pipeline/pandas/2-from_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""From File"""
+
+import pandas as pd
+
+
+def from_file(filename, delimiter):
+    """Loads data from a file as a pd.DataFrame:
+        filename is the file to load from
+        delimiter is the column separator
+        Returns: the loaded pd.DataFrame."""
