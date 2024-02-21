dab62d3863e190bf476b0a9660d35b237f48ed52
vigobin <vgobin@outlook.com>
2024-02-21 16:47:53 -0600

    adjust api url

--- pipeline/apis/3-first_launch.py
+++ pipeline/apis/3-first_launch.py
@@ -6,24 +6,28 @@ from datetime import datetime
 
 def get_launch_info():
     """Displays the first launch  from the SpaceX API."""
-    response = requests.get("https://api.spacexdata.com/v5/launches/")
+    response = requests.get("https://api.spacexdata.com/v4/launches")
     data = response.json()
 
-    # Get the required information
-    launch_name = data['name']
+    # Sort the launches by date
+    sorted_launches = sorted(data, key=lambda x: x['date_unix'])
+
+    # Get the first launch
+    first_launch = sorted_launches[0]
+
+    launch_name = first_launch['name']
     date = datetime.fromtimestamp(
-        data['date_unix']).strftime('%Y-%m-%d %H:%M:%S')
+        first_launch['date_unix']).strftime('%Y-%m-%d %H:%M:%S')
     rocket_response = requests.get(
-        "https://api.spacexdata.com/v5/rockets/{}".format(data['rocket']))
+        "https://api.spacexdata.com/v4/rockets/" + first_launch['rocket'])
     rocket_name = rocket_response.json()['name']
     launchpad_response = requests.get(
-        "https://api.spacexdata.com/v5/launchpads/{}".format(
-            data['launchpad']))
+        "https://api.spacexdata.com/v4/launchpads/" + first_launch[
+            'launchpad'])
     launchpad_data = launchpad_response.json()
     launchpad_name = launchpad_data['name']
     launchpad_locality = launchpad_data['locality']
 
-    # Print the information in the specified format
     print("{} ({}) {} - {} ({})".format(
         launch_name, date, rocket_name, launchpad_name, launchpad_locality))
 
