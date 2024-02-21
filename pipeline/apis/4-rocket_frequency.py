0eb29134932091935ed95b414d5f92087e2e07fc
vigobin <vgobin@outlook.com>
2024-02-21 16:35:38 -0600

    no. of launches

---
+++ pipeline/apis/4-rocket_frequency.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Task 4. How many by rocket?"""
+import requests
+from collections import defaultdict
+
+
+def get_launches_per_rocket():
+    """Script that displays the number of launches per rocket"""
+    launches_per_rocket = defaultdict(int)
+
+    response = requests.get("https://api.spacexdata.com/v3/launches")
+    data = response.json()
+
+    for launch in data:
+        rocket_name = launch['rocket']['rocket_name']
+        launches_per_rocket[rocket_name] += 1
+
+    sorted_rockets = sorted(
+        launches_per_rocket.items(), key=lambda x: (-x[1], x[0]))
+
+    for rocket, launches in sorted_rockets:
+        print(f"{rocket}: {launches}")
+
+
+if __name__ == '__main__':
+    get_launches_per_rocket()
