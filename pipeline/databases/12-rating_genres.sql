3f430d222b3c355da90bef35693c5778efca5805
vigobin <vgobin@outlook.com>
2024-02-26 15:21:40 -0600

    uppercase AS

--- pipeline/databases/12-rating_genres.sql
+++ pipeline/databases/12-rating_genres.sql
@@ -1,5 +1,5 @@
 -- Best genre
-SELECT tv_genres.name, SUM(rate) as rating
+SELECT tv_genres.name, SUM(rate) AS rating
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id
 JOIN tv_show_ratings ON tv_show_genres.show_id=tv_show_ratings.show_id
