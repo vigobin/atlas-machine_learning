15dfbe3cbf7182d327fa633521f8e09c72bc5152
vigobin <vgobin@outlook.com>
2024-02-26 16:24:45 -0600

    Task 17

---
+++ pipeline/databases/17-store.sql
@@ -0,0 +1,13 @@
+-- 17. Buy buy buy
+-- creates a trigger that decreases the quantity
+-- of an item after adding a new order.
+DELIMITER //
+CREATE TRIGGER after_order_insert
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+   UPDATE items
+   SET quantity = quantity - NEW.quantity
+   WHERE id = NEW.item_id;
+END; //
+DELIMITER ;
