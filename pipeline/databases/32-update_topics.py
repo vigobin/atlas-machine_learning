af8c0fd0f7fbf9a9308dad4617ba364f46a0e5f3
vigobin <vgobin@outlook.com>
2024-02-28 09:01:55 -0600

    try update_one

---
+++ pipeline/databases/32-update_topics.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Change school topics"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """Function that changes all topics of a school document based on the name:
+        mongo_collection will be the pymongo collection object.
+        name (string) will be the school name to update.
+        topics (list of strings) will be the list of topics
+            approached in the school."""
+    mongo_collection.update_one(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
